6eda7381a73135c36a34bf40d0967688a07c1fdb
yassine574 <yassinefa0102@gmail.com>
2024-05-20 22:29:45 +0100

    main.js

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-const menuBtn = document.getElementById("menu-btn");
-const navLinks = document.getElementById("nav-links");
-const menuBtnIcon = menuBtn.querySelector("i");
-
-menuBtn.addEventListener("click", (e) => {
-  navLinks.classList.toggle("open");
-
-  const isOpen = navLinks.classList.contains("open");
-  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
-});
-
-navLinks.addEventListener("click", (e) => {
-  navLinks.classList.remove("open");
-  menuBtnIcon.setAttribute("class", "ri-menu-line");
-});
-
 const scrollRevealOption = {
   distance: "50px",
   origin: "bottom",
@@ -51,10 +35,3 @@ ScrollReveal().reveal(".blog__card", {
   interval: 500,
 });
 
-const swiper = new Swiper(".swiper", {
-  loop: true,
-
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
